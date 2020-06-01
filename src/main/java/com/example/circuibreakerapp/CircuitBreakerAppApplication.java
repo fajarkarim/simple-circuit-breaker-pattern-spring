e656538ea7aec52121c7f5d8ce82907a680f6c1b
Fajar Karim <fajar.karim@btpn.com>
2020-06-01 22:55:24 +0700

    downgrade spring boot to 1.5.8

--- src/main/java/com/example/circuibreakerapp/CircuitBreakerAppApplication.java
+++ src/main/java/com/example/circuibreakerapp/CircuitBreakerAppApplication.java
@@ -3,11 +3,13 @@ package com.example.circuibreakerapp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableCircuitBreaker
+@EnableHystrixDashboard
 public class CircuitBreakerAppApplication {
 
     public static void main(String[] args) {
